63b971d2ae4a5f12a2b3f3e7ad7085621acacf61
amanjot-garcha-au19 <85173918+amanjot-garcha-au19@users.noreply.github.com>
2021-07-10 23:58:42 -0600

    Assignment week 06

--- assignments/week06/day5/searchinsertposition.py
+++ assignments/week06/day5/searchinsertposition.py
@@ -1,11 +1,30 @@
 def searchInsert(nums: list[int], target: int) -> int:
         
-        if target > nums[-1]:
-            return len(nums)
         
-        for i in range (len(nums)):
-            if target <= nums[i]:
-                return i
+        
+    start = 0
+    end = len(nums)-1
+    if target > nums[-1]:
+        return len(nums)
+    elif target < nums[0]:
+        return 0 
+    
+    
+    
+    mid = int((start + end)/2)
+        
+    if target == nums[mid]:
+            return mid
+        
+    elif target < nums[mid] :
+            for i in range(start,mid+1):
+                if target <= nums[i]:
+                    return i
+    else:
+            for i in range (mid,len(nums)):
+                if target <= nums[i]:
+                    return i
+    
 
-print(searchInsert([1,3,5,7,9],8))
-        
+print(searchInsert([1,3,5,7,9],8),end="")
+    
