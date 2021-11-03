1fc9df137b67eee5b6097f0a7a471983bdb0cfa7
amanjot-garcha-au19 <85173918+amanjot-garcha-au19@users.noreply.github.com>
2021-11-03 01:24:39 -0600

    Add files via upload

---
+++ coding-challenges/week24/datatypes.js
@@ -0,0 +1,20 @@
+var a = 500
+var b = "string"
+var c = true
+var f 
+var d = Symbol("id")
+var g = Math.pow(300,300)
+
+h = {
+    z :700,
+    x: "string",
+    y : false
+  
+}
+console.log("Type of a =  " + typeof a)
+console.log("Type of b =  " + typeof b)
+console.log("Type of c =  " + typeof c)
+console.log("Type of f =  " + typeof f)
+console.log("Type of d =  " + typeof d)
+console.log("Type of g =  " + typeof g)
+console.log("Type of h =  " + typeof h)
