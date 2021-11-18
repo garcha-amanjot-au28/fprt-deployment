5e0ce3f206fbe1121bb066fa83060f7e2592e9eb
amanjot-garcha-au19 <85173918+amanjot-garcha-au19@users.noreply.github.com>
2021-11-18 11:45:45 -0700

    CC week 26 day 02

---
+++ coding-challenges/week26/day02/sum.js
@@ -0,0 +1,13 @@
+// let Sumi = document.getele('#btn');
+
+
+            
+//             function dosum (){
+//                 var Num1 = parseInt(document.getElementById('num1').value);
+//                 var Num2 = parseInt(document.getElementById('num2').value);
+//                 var result = document.getElementById('result').value = Num1 + Num2;
+                
+                
+
+                // }
+            // Sumi.addEventListener('click' , dosum)
