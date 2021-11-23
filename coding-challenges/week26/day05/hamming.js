36bfcefab267407d5f94ad44fba44ae3b90835a8
amanjot-garcha-au19 <85173918+amanjot-garcha-au19@users.noreply.github.com>
2021-11-22 19:32:50 -0700

    CC week26 day05

---
+++ coding-challenges/week26/day05/hamming.js
@@ -0,0 +1,19 @@
+
+let find = document.querySelector('#submit')
+
+function hamming (){
+    let string1 = document.querySelector('#string1').value
+    let string2 = document.querySelector('#string2').value
+    
+    var count = 0;
+    
+    for(i = 0; i < string1.length; i++){
+        if (string1[i] !== string2[i]) count += 1;
+    }
+    
+    document.getElementById('res').value = count;
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+    find.addEventListener('click',hamming)
+});
